af5bd40c5a4ca61321fb16eed85424f27d4d870a
João Vitor <88462714+xjapax@users.noreply.github.com>
2022-08-19 22:05:01 -0300

    aula1

---
+++ prj001/src/App2.java
@@ -0,0 +1,22 @@
+public class App2{
+    public static void main( String[] args){
+        //  idade ano atual
+        int idade = 35;
+
+        //  idade ano que vem
+
+        int idadeAnoQueVem = idade + 1;
+        System.out.println("proxima idade" + idadeAnoQueVem);
+        
+
+
+        
+
+
+        
+
+
+
+    }
+
+}
---
+++ prj001/src/exemploplochar.java
@@ -0,0 +1,22 @@
+public class exemploplochar {
+   
+        public static void main(String[] args){
+            char letra = 'A';
+    
+            System.out.println(letra);
+            letra++;
+            System.out.println(letra);
+            int letraint = letra;
+            System.out.println(letraint);
+            String palavra = "mous\ne";
+            System.out.println(palavra);
+            palavra = "casa";
+            System.out.println(palavra);
+            palavra = palavra.toUpperCase();
+            System.out.println(palavra);
+            
+        }
+        
+    }
+    
+
